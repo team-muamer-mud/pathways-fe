5037d75ff2b298a665ac0343fb80230ab6c48bb3
Michael Harms <michael.harms6010@gmail.com>
2020-03-04 12:44:26 -0600

    basic map functionality - lines NEED  cleaning up l m a o

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,46 @@
 import React from 'react'
+import "react-vis/dist/style.css"
+
+import {
+     XYPlot,
+     MarkSeries,
+     LineMarkSeries,
+     VerticalGridLines,
+     HorizontalGridLines,
+} from "react-vis";
 
 function Map({world, currentRoom}){
+     const corners = [
+          { x: 10, y: 10 },
+          { x: -1, y: 10 },
+          { x: 10, y: -1 },
+          { x: -1, y: -1 },
+        ];
      console.log(world)
      console.log(currentRoom)
 
      // className = {item.id === currentRoom.id ? "active" : ""}
      return (
           <div className="game-visual-container">
-               Hello I am Map
-               <div>{world.map((item) => {
-                    return (<div className={item.id === currentRoom.id ? "active" : "inactive"}>{item.title}</div>)
-               })}
-               </div>
+               <XYPlot height={600} width={600}>
+                    <LineMarkSeries 
+                         lineStyle={{ stroke: 'black', strokeWidth: '3px' }}
+                         data={world} 
+                    />
+
+                    <MarkSeries
+                         strokeWidth={18}
+                         data={[{ x: currentRoom.x, y: currentRoom.y}]}
+                         color="orange"
+                    />
+
+                    <MarkSeries
+                         className="corners"
+                         strokeWidth={0.5}
+                         data={corners}
+                    />
+
+               </XYPlot>
           </div>)
 }
 
