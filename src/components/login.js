9b4c8ba9369576492d7ac4a71786124db8351dd9
Gatrrr <davidgatrmacaulay@gmail.com>
2020-03-04 13:31:37 -0800

    Readded Login/Reg

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { axiosWithAuth } from '../utils/axioswithAuth';
 import { Link } from 'react-router-dom';
+//
 
 const Login = props => {
 
